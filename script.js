62f7451a4d8290d9efeacafb284ea3517cf90342
luixx2008 <sr.vicious@outlook.com>
2026-05-18 10:10:44 -0600

    18/5/26

--- script.js
+++ script.js
@@ -47,11 +47,63 @@ btnEnviarTarea.addEventListener('click', () => {
     const nuevaTarea = tareaInput.value
     // Crear un nuevo elemento de lista
     const nuevaTareaElemento = document.createElement('li')
-    // Asignar el texto de la nueva tarea al elemento
-    nuevaTareaElemento.textContent = nuevaTarea
+    nuevaTareaElemento.innerHTML = `
+        <span>${nuevaTarea}</span>
+        <button class="btnEliminar">Eliminar</button>
+    `;
+
     // Agregar el elemento a la lista
     tareaList.appendChild(nuevaTareaElemento)
     // Limpiar el input
     tareaInput.value = ''
     }})
 
+    //delegacion de eventos
+    tareaList.addEventListener('click', (event) => {
+        //1 detectar si se hizo click en el boton eliminar
+        if (event.target.classList.contains('btnEliminar')) {
+            const lipadre = event.target.parentElement;
+            lipadre.remove()
+        }
+        //2 detectar si se hizo click en el texto de la tarea o el li para marcarla como completada
+        else {
+            //buscar el li mas cercano por si se hizo click en el span o en el li
+            const li = event.target.closest('li')
+            if (li) li.classList.toggle('tareaCompletada');
+        }
+    });
+const btnEliminarTarea = document.getElementById('btnEliminarTarea')
+btnEliminarTarea.addEventListener('click', () => {
+    // Eliminar todas las tareas de la lista
+    tareaList.innerHTML = '';
+})
+function guardarEnStorage() {
+    const tareas = [];
+    //recorrer los li y extraer su info
+    tareaList.querySelectorAll('li').forEach(li => {
+        tareas.push({
+            texto: li.querySelector('span').textContent,
+            completada: li.classList.contains('tareaCompletada')
+        })
+    });
+    //guardar el array convertido a string
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+}
+//funcion cargar
+function cargarTareas() {
+    const tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || [];
+    tareasGuardadas.forEach(tarea => {
+        crearElementoTarea(tarea.texto, tarea.completada);
+    });
+}
+
+//funcion auxiliar para no repetir codigo al crear el html
+function crearElementoTarea(texto, completada = false) {
+    const nuevaTareaElemento = document.createElement('li')
+    if (completada) nuevaTareaElemento.classList.add('tareaCompletada');
+    nuevaTareaElemento.innerHTML = `
+        <span>${texto}</span>
+        <button class="btnEliminar">Eliminar</button>
+    `;
+    tareaList.appendChild(nuevaTareaElemento)
+}
